2a7c6ac9b413542ab12369ff3b581c50f868935a
najah <najahaboobacker79@gmail.com>
2024-03-03 00:39:36 +0530

    api prob

--- src/app/code-editor/page.jsx
+++ src/app/code-editor/page.jsx
@@ -1,11 +1,23 @@
+"use client"
+
 import CodeEditor from "@/components/CodeEditor";
 import  Navbar  from "@/components/Navbar";
 
 export default function page(){
+
+    const data = async () =>{
+        const res = await fetch('http://localhost:3000/api/codeEditor')
+        console.log("testing",res.json())
+    }
+    data();
+
+    const getCode = (a) => {
+        console.log("received data from client",a)
+    }
     return(
         <div>
             <Navbar/>
-            <CodeEditor/>
+            <CodeEditor getCode={getCode} />
         </div>
     )
 }
